5c936b347d100e5cfcd651f7de8289a9efab7671
Mates061011 <161950987+Mates061011@users.noreply.github.com>
2024-12-05 12:51:55 +0100

    qrPay delete from frontend

--- frontend/src/pages/cart/Cart.tsx
+++ frontend/src/pages/cart/Cart.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import './cart.css';
-import PaymentQR from '../../components/qrPay';
 interface UserData {
   name: string;
   surname: string;
