f329b704952eff2f9ff16ad87f6cc7d045c6c1b6
Mates061011 <161950987+Mates061011@users.noreply.github.com>
2025-01-02 20:17:35 +0100

    vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // If the module is from node_modules, bundle it into a 'vendor' chunk
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
