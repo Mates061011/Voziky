3d4bfe1a5a3267a2a2426274f19a226769b9ebce
Mates061011 <161950987+Mates061011@users.noreply.github.com>
2025-02-11 22:14:32 +0100

    fix moznosti

--- frontend/src/pages/mainpage/Mainpage_section4b/MainPage_section4b.tsx
+++ frontend/src/pages/mainpage/Mainpage_section4b/MainPage_section4b.tsx
@@ -4,7 +4,11 @@ import './styles.css';
 const MainPage_section4b = () => {
   return (
     <div className="prislusenstviWrap">
-      <Items type="special"/>
+        <div className="nadpis-cart">
+            <h4>VÍC MOŽNOSTÍ</h4>
+            <h3>Příslušenství</h3>
+        </div>
+        <Items type="special"/>
     </div>
   )
 }
