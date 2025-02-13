ad5991775d31c506904e92b0fe62145c080aa251
Mates061011 <161950987+Mates061011@users.noreply.github.com>
2025-02-13 14:56:30 +0100

    fix responsivity

--- frontend/src/pages/mainpage/Mainpage_section3/Mainpage-section3.tsx
+++ frontend/src/pages/mainpage/Mainpage_section3/Mainpage-section3.tsx
@@ -18,13 +18,19 @@ export default function Section3() {
       <div className="popis-a-kocarek">
         <div className="popis-cont flexColumn">
             <span className={`para ${isExpanded ? 'paraExpanded' : ''}`}>
-                dvoumístný multifunkční vozík za kolo a kočárek pro kondiční běh v jednom.
-                Thule Chariot Lite 1 Agave - výlet do světa snů pro nejmenší, nabízející bezpečí, pohodlí a špičkový design.
+                dvoumístný multifunkční vozík za kolo a kočárek pro kondiční běh v jednom. <br /><br />
+                Thule Chariot Lite 1 Agave - výlet do světa snů pro nejmenší, nabízející bezpečí, pohodlí a špičkový design. <br /><br />
                 Thule Chariot Lite 1 Agave 2021 je jako kouzelný kompas, který vaše rodinná dobrodružství směřuje k nekonečným cyklistickým zážitkům. S elegancí designu vám tento dětský vozík otevírá dveře do světa vzrušení
                 <span className="readMoreText" onClick={toggleText}>
                     {isExpanded ? 'Zobrazit méně' : 'Zobrazit více'}
                 </span>
             </span>
+            <div className="pujceni-info">
+              <p>Cena za vypůjčení za 1 den: 300 Kč</p>
+            </div>
+            <div className="pujceni-info">
+              <p>Cena za vypůjčení za 2 a více dní: 250 Kč</p>
+            </div>
         </div>
         <img src="/items/Thule Chariot Sport 2 double 02.png" alt="Thule Chariot Lite 1 Agave" loading="lazy" />
       </div>
--- frontend/src/pages/mainpage/Mainpage_section6/Mainpage-section6.tsx
+++ frontend/src/pages/mainpage/Mainpage_section6/Mainpage-section6.tsx
@@ -3,17 +3,19 @@ import Faq from '../../../components/faq/Faq';
 import ContactForm from '../../../components/faq/Faq-form';
 
 const Section6 = () => (
-  <div className='faq-main-cont'>
-    <div className="nadpis-section6">
-        <h4>FAQ</h4>
-        <h3>Často kladené dotazy</h3>
-      </div>
-    <div className='flexRow'>
-      <div className='questions flexColumn'>
-        <Faq />
-      </div>
-      <div className='qaa-form flexColumn'>
-        <ContactForm />
+  <div className="faqWrap">
+    <div className='faq-main-cont'>
+      <div className="nadpis-section6">
+          <h4>FAQ</h4>
+          <h3>Často kladené dotazy</h3>
+        </div>
+      <div className='flexRow'>
+        <div className='questions flexColumn'>
+          <Faq />
+        </div>
+        <div className='qaa-form flexColumn'>
+          <ContactForm />
+        </div>
       </div>
     </div>
   </div>
