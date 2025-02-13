d901e110b11fc75131b8c90b02e020e96f6f7c1a
Mates061011 <161950987+Mates061011@users.noreply.github.com>
2025-02-13 15:59:11 +0100

    responsivity

--- frontend/src/pages/mainpage/Mainpage_section7/Mainpage_section7.tsx
+++ frontend/src/pages/mainpage/Mainpage_section7/Mainpage_section7.tsx
@@ -1,65 +1,48 @@
 import { useState } from 'react';
 import './mainpage_section7.css';
-import { Container } from '@mui/material';
-import Grid from '@mui/material/Grid';
 import NaVetviLogo from '../../../assets/NaVetviLogo.svg';
 
 const Section7 = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleText = () => {
-    setIsExpanded(!isExpanded);
+    setIsExpanded((prev) => !prev);
   };
 
   return (
-    <Container className="section7">
-      <Grid container spacing={{ xs: 2, sm: 5 }}>
-        {/* First Grid Item */}
-        <Grid item xs={12} sm={6} md={7}>
-          <div className="gridWrap">
+    <div className="section7">
+      <div className="section7Wrap">
+        {/* Section Title and Image */}
+        <div className="section7-header">
+          <div className="section7-nadpis">
             <h4>NAŠE TIPY NA VÝLETY PO OKOLÍ</h4>
             <h3>Park Na větvi</h3>
-            <span className={`para ${isExpanded ? 'paraExpanded' : ''}`}>
-              Park Na větvi tvoří unikátní soustava tří síťových hřišť zavěšených v korunách stromů propojených s 3D bludištěm, které je složeno ze síťových překážkových drah rozprostřených ve třech patrech nad sebou. Síťové bludiště v korunách stromů obsahuje mnoho zábavných prvků, které otestují vaše motorické dovednosti. Jednotlivé herní plochy jsou propojeny visutými lávkami. Celá atrakce vrcholí sjezdem unikátním tobogánem z výšky necelých 10 m. Pohybová atrakce je natolik bezpečná, že na rozdíl od klasických lanových center nevyžaduje žádné osobní lanové jištění. <br />
-              Součástí areálu je plně vybavený stánek s občerstvením, ve kterém se mohou občerstvit malí dobrodruzi i čekající doprovod. Nabízí se zde regionální speciality od místních dodavatelů a o víkendech se před stánkem grilují sezónní dobroty.
+          </div>
+          <img src={NaVetviLogo} alt="Logo Na Vetvi" className="NaVetviPic" />
+        </div>
 
+        {/* Text Content and Details */}
+        <div className="contentWrap">
+          <div className="gridWrap">
+            <span className={`para ${isExpanded ? 'paraExpanded' : ''}`}>
+              Park Na větvi tvoří unikátní soustava tří síťových hřišť zavěšených v korunách stromů propojených s 3D bludištěm, které je složeno ze síťových překážkových drah rozprostřených ve třech patrech nad sebou. Síťové bludiště v korunách stromů obsahuje mnoho zábavných prvků, které otestují vaše motorické dovednosti. Jednotlivé herní plochy jsou propojeny visutými lávkami. Celá atrakce vrcholí sjezdem unikátním tobogánem z výšky necelých 10 m. Pohybová atrakce je natolik bezpečná, že na rozdíl od klasických lanových center nevyžaduje žádné osobní lanové jištění.
+              <br />
+              Součástí areálu je plně vybavený stánek s občerstvením, ve kterém se mohou občerstvit malí dobrodruzi i čekající doprovod. Nabízí se zde regionální speciality od místních dodavatelů a o víkendech se před stánkem grilují sezónní dobroty.
               <span className="readMoreText" onClick={toggleText}>
                 {isExpanded ? 'Zobrazit méně' : 'Zobrazit více'}
               </span>
             </span>
-            <div style={{ marginTop: '40px' }}>
-              <p><b>vzdálenost od půjčovny:</b> 2km</p>
-              <p><b>vhodné pro:</b> pěší, cyklisty, in-line, bruslaře, běžce</p>
-              <p><b>web:</b> www.parknavetvi.cz</p>
+            <div className="details" style={{ marginTop: '40px' }}>
+              <p><b>Vzdálenost od půjčovny:</b> 2 km</p>
+              <p><b>Vhodné pro:</b> pěší, cyklisty, in-line bruslaře, běžce</p>
+              <p><b>Web:</b> <a href="http://www.parknavetvi.cz" target="_blank" rel="noopener noreferrer">www.parknavetvi.cz</a></p>
             </div>
           </div>
-        </Grid>
+          <div className="imgWrap2"></div>
+        </div>
 
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          md={5}
-          sx={{
-            display: { xs: 'flex', sm: 'block' },
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Grid item xs={12} sm={4} className="imgWrap">
-            <img src={NaVetviLogo} alt="Logo Na Vetvi" className="NaVetviPic" />
-          </Grid>
-          <Grid item xs={12} sm={8} className="imgWrap"
-          sx={{
-            display: { xs: 'flex', sm: 'block' },
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-            <div className="imgWrap2"></div>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
+      </div>
+    </div>
   );
 };
 
